6a4d0250da878a0bd9f483c76a2944b9f467b2ff
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2023-06-24 18:11:23 +0100

    feat: later file upload

---
+++ as.ts
@@ -0,0 +1,71 @@
+import express, { NextFunction } from "express";
+import formidable from "formidable";
+import fs from "fs";
+import IncomingForm from "formidable/Formidable";
+import { IncomingMessage } from "http";
+import { Essentia, EssentiaWASM } from "essentia.js";
+import decode from "audio-decode";
+
+const app = express();
+const port = 3000;
+
+const essentia = new Essentia(EssentiaWASM);
+
+const KEYS = ["C", "D", "E", "F", "G", "A", "B"];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const parseForm = async (
+    form: IncomingForm,
+    req: IncomingMessage,
+    next: NextFunction
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+    return await new Promise((resolve) => {
+        form.parse(req, function (err, fields, files) {
+            if (err) return next(err);
+            resolve({ fields, files });
+        });
+    });
+};
+
+const decodeAudio = async (filepath: string) => {
+    const buffer = fs.readFileSync(filepath);
+    const audio = await decode(buffer);
+    const audioVector = essentia.arrayToVector(audio._channelData[0]);
+    return audioVector;
+};
+
+app.post("/upload", async (req, res, next) => {
+    const form = formidable();
+
+    const { files } = await parseForm(form, req, next);
+    const file = await files.file;
+
+    const data = await decodeAudio(file.path);
+
+    const danceability = essentia.Danceability(data).danceability;
+    const duration = essentia.Duration(data).duration;
+    const energy = essentia.Energy(data).energy;
+
+    const computedKey = essentia.KeyExtractor(data);
+    const key = KEYS.indexOf(computedKey.key);
+    const mode = computedKey.scale === "major" ? 1 : 0;
+
+    const loudness = essentia.DynamicComplexity(data).loudness;
+    const tempo = essentia.PercivalBpmEstimator(data).bpm;
+
+    res.status(200).json({
+        danceability,
+        duration,
+        energy,
+        key,
+        mode,
+        loudness,
+        tempo,
+    });
+});
+
+app.listen(port, () => {
+    return console.log(`Express server listening at http://localhost:${port}`);
+});
